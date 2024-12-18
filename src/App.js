5c9c08ef155698afd953ec6a28946537633c0462
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-12-18 11:54:41 +0000

    Migrated fetch code to cart-slice and created own action creater thunk

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect, Fragment } from "react";
-import { uiActions } from "./store/ui-slice";
+
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -16,46 +17,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    async function sendCartData() {
-      dispatch(
-        uiActions.showNotification({
-          status: "Pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://advanced-redux-928c7-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "Success",
-          title: "Success...",
-          message: "Sent cart data successfully!",
-        })
-      );
-    }
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
