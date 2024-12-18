b6a8af363be37c6d6b453de6774242a500187d5f
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-12-18 11:27:56 +0000

    Linked the project to a Firebase backend to store cart data. Also displaying if the fetch request was successful

--- src/App.js
+++ src/App.js
@@ -1,15 +1,77 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect, Fragment } from "react";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisable);
+  const cart = useSelector((state) => state.cart);
+
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    async function sendCartData() {
+      dispatch(
+        uiActions.showNotification({
+          status: "Pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://advanced-redux-928c7-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "Success",
+          title: "Success...",
+          message: "Sent cart data successfully!",
+        })
+      );
+    }
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,13 +1,15 @@
 import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
-import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
   const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
 
-  function addToCartHandler() {
+  const addToCartHandler = () => {
     dispatch(
       cartActions.addItemToCart({
         id,
@@ -15,7 +17,18 @@ const ProductItem = (props) => {
         price,
       })
     );
-  }
+
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    // dispatch(
+    //   cartActions.addItemToCart({
+    //     id,
+    //     title,
+    //     price,
+    //   })
+    // );
+  };
 
   return (
     <li className={classes.item}>
